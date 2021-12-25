a428fed6834cb42af017447853e07a93a7fb190a
Nursultan Askarbekuly <nursultan@outlook.com>
2021-12-25 19:07:13 +0300

    add title, change button frame and title

--- DonationKitDemo/ViewController.swift
+++ DonationKitDemo/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
 
     lazy var purchaseButton: UIButton = {
         let button = UIButton(type: .system)
-        button.frame = CGRect(x: view.bounds.width/2 - 125, y: view.bounds.height/2 - 25, width: 250, height: 50)
-        button.setTitle("Donate", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        button.frame = CGRect(x: view.bounds.width/2 - 125, y: view.bounds.height/2 - 22, width: 250, height: 44)
+        button.setTitle("Launch", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 19, weight: .semibold)
         button.setTitleColor(.systemBackground, for: .normal)
         button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 8
+        button.layer.cornerRadius = 5
         button.addTarget(self, action: #selector(showDonationScreen), for: .touchUpInside)
         return button
     }()
@@ -26,6 +26,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(purchaseButton)
+        
+        self.title = "Client App"
+        self.navigationItem.backButtonTitle = ""
     }
 
     @objc func showDonationScreen() {
@@ -33,9 +36,10 @@ class ViewController: UIViewController {
         let configuration = PurchaseConfiguration(
             id: "standard",
             purchaseProductIdentifiers:
-                ["com.demo.price01",
-                 "com.demo.price05",
-                 "com.demo.price10"
+                [
+                    "com.demo.price01",
+                    "com.demo.price05",
+                    "com.demo.price10"
                 ]
         )
         
